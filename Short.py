9fd3453c09a9793e0ec5fb01a929edf7859c838f
Arpanka <87801076+Arpanka@users.noreply.github.com>
2021-07-25 13:30:21 +0600

    Update Short.py

--- Short.py
+++ Short.py
@@ -1,8 +1,11 @@
 
 #!/data/data/com.termux/files/usr/bin/python3
+
 #imprort module
+
 import pyshorteners
 #Take input from user
+
 link = input("Enter a url: ")
 short  = pyshorteners.Shortener()
 gurl = short.tinyurl.short(link)
