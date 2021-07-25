d43fbde9b203e9e8cb1d28d7392dd0dd65f7a39f
Arpanka <87801076+Arpanka@users.noreply.github.com>
2021-07-25 13:22:34 +0600

    Create Short.py

---
+++ Short.py
@@ -0,0 +1,10 @@
+
+#!/data/data/com.termux/files/usr/bin/python3
+#imprort module
+import pyshorteners
+#Take input from user
+link = input("Enter a url: ")
+short  = pyshorteners.Shortener()
+gurl = short.tinyurl.short(link)
+print("your short url is : ")
+print(gurl)
